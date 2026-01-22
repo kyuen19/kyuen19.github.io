1954ef430a94eb3576ef6048e40bfe2a364caeb4
Robert Sayre <sayrer@gmail.com>
2026-01-22 10:42:31 -0800

    Leave comment in.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kyuen19.github.io",
+  // If deploying to a repo other than username.github.io, set base:
+  // base: '/your-repo-name',
 });
